5d930b981d3458285d15e92a555c5ad46e1ff4b1
Guzio <guzekk@protonmail.com>
2026-08-02 19:03:20 +0200

    Push before I probably make a mess by adding KTor.

--- src/main/kotlin/hub/guzio/serveryor/Main.kt
+++ src/main/kotlin/hub/guzio/serveryor/Main.kt
@@ -1,20 +1,34 @@
 package hub.guzio.serveryor
 
+import folk.sisby.surveyor.WorldSummary
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents
+import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.Level
 import org.slf4j.LoggerFactory
 
 object Main : ModInitializer {
 	const val MOD_ID: String = "serveryor"
 
 	private val LOGGER = LoggerFactory.getLogger(MOD_ID)
+	private val LEVELS = HashMap<ResourceKey<Level>, Level>()
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Serveryor is registering events....")
+		ServerWorldEvents.LOAD.register { _, level ->
+			LOGGER.info("Serveryor detected a new: "+level.dimension())
+			LEVELS[level.dimension()] = level
+		}
+		ServerWorldEvents.UNLOAD.register { _, level ->
+			LOGGER.info("Serveryor is unloading: "+level.dimension())
+			LEVELS.remove(level.dimension())
+		}
 
-		LOGGER.info("Hello Fabric world!")
+		LOGGER.info("Booting up the (web)server(yor)....")
+		//TODO
+
+		LOGGER.info("Serveryor is done!")
 	}
 
 	fun id(path: String): ResourceLocation
