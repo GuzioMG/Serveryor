375e7901e2b2257f8efbb301b04abdca7c1dbb26
Guzio <guzekk@protonmail.com>
2026-08-03 22:16:12 +0200

    Global depth scaling

--- src/main/kotlin/hub/guzio/surwebyor/DataGetter.kt
+++ src/main/kotlin/hub/guzio/surwebyor/DataGetter.kt
@@ -1,35 +1,31 @@
 package hub.guzio.surwebyor
 
 import folk.sisby.surveyor.WorldSummary
-import io.nayuki.png.PngImage
 import io.nayuki.png.image.BufferedGrayImage
 import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.Level
-import java.io.ByteArrayOutputStream
-import java.io.OutputStream
 import java.util.Objects
 
 object DataGetter {
+    const val FIXED_POINT_PRECISION = 100
+
     fun getImgOfChunk(dim: Level, coords: ChunkPos, zoom: Int): BufferedGrayImage? {
+
         val map = WorldSummary.of(dim)
-
-        val depthmap = map.terrain()?.get(coords)?.toSingleLayer(dim.minBuildHeight, dim.maxBuildHeight, dim.height)?.depths
+        val min = dim.minBuildHeight
+        val max = dim.maxBuildHeight
+        val depthmap = map.terrain()?.get(coords)?.toSingleLayer(min, max, max)?.depths
         if (Objects.isNull(depthmap)) return null
-        var min = Integer.MAX_VALUE
-        var max = Integer.MIN_VALUE
 
-        for (depth in depthmap!!) {
-            min = min.coerceAtMost(depth)
-            max = max.coerceAtLeast(depth)
-        }
-
-        val scaledMax = max-min
-        val scalingFactor = 255 / scaledMax
+        val scalingFactor = 255*FIXED_POINT_PRECISION / (max-min)
         val img = BufferedGrayImage(16, 16, arrayOf(8, 0).toIntArray())
 
-        for ((index, depth) in depthmap.withIndex()) {
-            val depthScaled = (depth-min)*scalingFactor
-            val pixelData = depthScaled.shl(16)
+        for ((index, depth) in depthmap!!.withIndex()) {
+            val yLevel = max - depth
+            val yFromBottom = yLevel-min
+            val yScaled = yFromBottom*scalingFactor/FIXED_POINT_PRECISION
+            println("Found block at YLevel=$yLevel, which is $yFromBottom blocks up, or $yScaled when multiplied by $scalingFactor")
+            val pixelData = yScaled.shl(16)
             img.setPixel(index/16, index%16, pixelData)
         }
 
